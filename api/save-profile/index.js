3208fd5418d2f03bf9fe28c49d90b166f1765ede
kazuya-design10 <coca.cococo@gmail.com>
2026-08-28 15:22:14 +0900

    Update index.js

--- api/save-profile/index.js
+++ api/save-profile/index.js
@@ -1,1 +1,17 @@
 
+module.exports = async function (context, req) {
+
+  const {
+    name,
+    age,
+    city,
+    user_id
+  } = req.body;
+
+  // Auth0更新
+
+  context.res = {
+    status: 200,
+    body: "ok"
+  };
+};
