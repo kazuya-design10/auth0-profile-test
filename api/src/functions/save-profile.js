272eb9d8583e66209bfd7420cb16b0a90b76ad73
kazuya-design10 <coca.cococo@gmail.com>
2026-08-31 17:26:11 +0900

    Update save-profile.js

--- api/src/functions/save-profile.js
+++ api/src/functions/save-profile.js
@@ -1,5 +1,6 @@
 const { app } = require("@azure/functions");
 
+
 app.http("save-profile", {
   methods: ["GET", "POST"],
   authLevel: "anonymous",
@@ -10,17 +11,38 @@ app.http("save-profile", {
     if (request.method === "POST") {
       try {
         body = await request.json();
+
+const jwt = require("jsonwebtoken");
+        const responseToken =　jwt.sign({
+          state: body.state,
+          other: {
+            first_name: body.first_name,
+            last_name: body.last_name,
+            age: body.age,
+            city: body.city
+          }
+        },
+        process.env.SESSION_TOKEN_SECRET,
+        {
+          expiresIn: "5m"
+        });
+        
       } catch (error) {
         context.log("JSON parse failed:", error.message);
       }
-    }
+    
 
     return {
       status: 200,
       jsonBody: {
-        message: "hello",
-        body: body
-      }
-    };
+          continueUrl:
+          `https://${process.env.AUTH0_DOMAIN}/continue?state=${body.state}`,
+          sessionToken:responseToken
+        }
+      };
+    }
   }
 });
+
+
+
