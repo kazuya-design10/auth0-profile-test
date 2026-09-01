32888ff98d5027b265114783a73c138891be8e6f
kazuya-design10 <coca.cococo@gmail.com>
2026-09-01 13:33:36 +0900

    Update save-profile.js

--- api/src/functions/save-profile.js
+++ api/src/functions/save-profile.js
@@ -1,34 +1,39 @@
 const { app } = require("@azure/functions");
 const jwt = require("jsonwebtoken");
-const responseToken = jwt.sign({
-      state: body.state,
-      other: {
+
+app.http("save-profile", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    if (request.method === "POST") {
+      try {
+        const body = await request.json();
+        const incomingToken = jwt.verify(
+        body.session_token,
+        process.env.SESSION_TOKEN_SECRET
+        );
+
+        const responseToken = jwt.sign({
+          state: body.state,
+          other: {
             first_name: body.first_name,
             last_name: body.last_name,
             age: body.age,
             city: body.city
-      }
-},
-        process.env.SESSION_TOKEN_SECRET,
-        {
-          expiresIn: "5m"
-        }
+          }
+        },
+    process.env.SESSION_TOKEN_SECRET,{
+      expiresIn: "5m"
+    }
 );
-
-app.http("save-profile", {
-  methods: ["GET", "POST"],
-  authLevel: "anonymous",
-
-  handler: async (request, context) => {
-    let body = null;
-
-    if (request.method === "POST") {
-      try {
-        body = await request.json();
-
-
-
-        
+        return {
+          status: 200,
+          jsonBody: {
+          continueUrl:
+          `https://${process.env.AUTH0_DOMAIN}/continue?state=${body.state}`,
+          sessionToken:responseToken
+          }
+        };
       } catch (error) {
         context.log("JSON parse failed:", error.message);
         return {
@@ -38,16 +43,6 @@ app.http("save-profile", {
                 }
                 };
         }
-    
-
-    return {
-      status: 200,
-      jsonBody: {
-          continueUrl:
-          `https://${process.env.AUTH0_DOMAIN}/continue?state=${body.state}`,
-          sessionToken:responseToken
-        }
-      };
     }
   }
 });
